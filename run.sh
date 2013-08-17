6ad004bed6c0ff901c065b4869260e4ebd428bc0
travisbot <support@travis-ci.org>
2013-08-17 23:08:31 +0530

    Updating credential file's location

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ git config --global user.email "support@travis-ci.org"
 git config --global user.name "travisbot"
 git config --global push.default current
 git config credential.helper "store --file=.git/credentials"
-echo "https://$GH_TOKEN:@github.com" > .git/credentials
+echo "https://$GH_TOKEN:@github.com" > .git/modules/github-master/credentials
 
 #Generating  Data
 echo ">> Generating Data"
