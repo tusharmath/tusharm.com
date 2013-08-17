a1833837a3e541498f94de2f8b7558b39bdf0c48
travisbot <support@travis-ci.org>
2013-08-17 20:34:35 +0530

    Adding github-master in gitignore

--- run.sh
+++ run.sh
@@ -1,40 +1,34 @@
-#Remove GH Pages Folder
 GH_TOKEN="d5ab1906e27ad4a3cbd32743fda35b71336f663a"
-echo ">> Clearing folder"
+
+#Generating  Data
+echo ">> Generating Data"
+git checkout develop
+wintersmith build --output='bin'
+rm -rf .git
+
+
+#Setup Master Branch
+echo ">> Setup Master branch"
 rm -rf .github-master
-
-#Clone Repo using https
-echo ">> Cloning repo"
 git clone "https://github.com/tusharmath/tusharmath.github.io.git" .github-master
-
-#Remove files
-echo ">> Removing all old files"
 rm -rfv .github-master/
 
-#Wintersmith build
-echo ">> Wintersmith Build"
-wintersmith build --output='bin'
 
 #Copy files
 echo ">> Copying files"
 cp -r bin/ .github-master
 
-echo "Change Dir to .github-master"
-cd .github-master
 
-echo ">> Setting up user details"
+#Prepare Commit
+echo ">> Preparing to commit"
+cd .github-master
 git config user.email "support@travis-ci.org"
 git config user.name "travisbot"
-
-echo ">> Preparing to commit"
-git add -all .
-
-echo ">> Comitting"
+git add --all .
 git commit -m "deploy from Travis"
-
-echo ">> Saving Credentials"
 git config credential.helper "store --file=.git/credentials"
 echo "https://$GH_TOKEN:@github.com" > .git/credentials
 
+#Pushing
 echo ">> Pushing to Git Hub"
 git push
