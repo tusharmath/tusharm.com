7107f96d9238efa1d9fcf30fe271208673482e33
travisbot <support@travis-ci.org>
2013-08-17 23:37:36 +0530

    Added git checkout master force commdna

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ git config --global user.email "support@travis-ci.org"
 git config --global user.name "travisbot"
 git config --global push.default current
  	
-echo "https://$GH_TOKEN:@github.com" > .git/modules/github-master/credentials
+echo "https://$GH_TOKEN:@github.com" > .git/credentials
 
 #Generating  Data
 echo ">> Generating Data"
@@ -12,7 +12,8 @@ git checkout develop
 
 #Change dir
 cd github-master
-git config credential.helper "store --file=../.git/modules/github-master/credentials"
+git checkout master --force
+git config credential.helper "store --file=../.git/credentials"
 
 #Prepare Commit
 echo ">> Preparing to commit"
