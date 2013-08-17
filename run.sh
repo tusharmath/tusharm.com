ef66f0f067f137797e3e7c095a42f6ea000c7798
Tushar Mathur <tusharmath@gmail.com>
2013-08-18 00:43:11 +0530

    Minor fixes

--- run.sh
+++ run.sh
@@ -1,24 +1,27 @@
 #Configuration
 git config --global user.email "support@travis-ci.org"
 git config --global user.name "travisbot"
-git config --global push.default current
- 	
+git config --global push.default current 	
 echo "https://$GH_TOKEN:@github.com" > .git/credentials
 
-#Generating  Data
-echo ">> Generating Data"
-git checkout develop
-./node_modules/wintersmith/bin/wintersmith build --output='github-master'
-
-#Change dir
+#Checkout master
 cd github-master
 git checkout master --force
 git config credential.helper "store --file=../.git/credentials"
 
+
+#Generating  Data
+cd ..
+echo ">> Generating Data"
+git checkout develop
+./node_modules/wintersmith/bin/wintersmith build --output='github-master'
+
+
 #Prepare Commit
+cd github-master
 echo ">> Preparing to commit"
 git add --all
-echo ">> Git Status"
+echo ">> Git Status"	
 git status
 git commit -m "deploy from Travis [ci skip]"
 
