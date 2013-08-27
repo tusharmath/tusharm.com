a6018a981ec587f2f3c0a741bd2bc39fd6244422
Tushar Mathur <tusharmath@gmail.com>
2013-08-27 20:43:59 +0530

    Updating Analytics

--- contents/js/analytics.js
+++ contents/js/analytics.js
@@ -1,16 +1,22 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-40580825-1']);
-_gaq.push(['_trackPageview']);
-(function() {
-	var ga = document.createElement('script');
-	ga.type = 'text/javascript';
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
-})();
+//Google Analytics
+
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o),
+	m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-39050808-2', 'tusharmath.github.io');
+ga('send', 'pageview');
 
 
+//TWITTER
 (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0],
 		p = /^http:/.test(d.location) ? 'http' : 'https';
