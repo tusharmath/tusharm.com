f6231c409242be28f1148b1a9639547baf8b08c7
Tushar Mathur <tusharmath@gmail.com>
2013-09-17 15:47:55 +0530

    trying

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,13 @@ PUBLISH_PATH="./build"
 BRANCH="master"
 WINTERSMITH="./node_modules/wintersmith/bin/wintersmith"
 
-#Clear Folder
-rm -rf $PUBLISH_PATH
-mkdir $PUBLISH_PATH
+#Add Global Credentials
+git config --global user.email "tusharmath@gmail.com"
+git config --global user.name "Travis-CI"
+
+#Set up authentication
+echo "https://$HEROKU_API_KEY:@github.com" > .git/credentials
+git config credential.helper "store --file=.git/credentials"
 
 #Clone Repositoy
 git clone $TO_REPOSITORY $PUBLISH_PATH --branch $BRANCH --verbose --single-branch
@@ -16,21 +20,10 @@ git clone $TO_REPOSITORY $PUBLISH_PATH --branch $BRANCH --verbose --single-branc
 rm -r $PUBLISH_PATH/*
 wintersmith build -o $PUBLISH_PATH
 
-#Remove upper .git folder
-rm -rf ./.git
 
 #Change dir
 cd $PUBLISH_PATH
 git add --all . --verbose
-
-#Add Global Credentials
-git config --global user.email "tusharmath@gmail.com"
-git config --global user.name "Travis-CI"
-echo "https://$HEROKU_API_KEY:@github.com" > .git/credentials
-
-#Set up authentication
-git config credential.helper "store --file=.git/credentials"
-
 
 
 #Commit and push
