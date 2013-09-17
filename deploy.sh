1b03b09558999d5648fb0bded9cfac345fbd03f2
Tushar Mathur <tusharmath@gmail.com>
2013-09-17 16:01:56 +0530

    trying

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,30 @@ PUBLISH_PATH="./build"
 BRANCH="master"
 WINTERSMITH="./node_modules/wintersmith/bin/wintersmith"
 
-#Add Global Credentials
+echo "Add Global Credentials"
 git config --global user.email "tusharmath@gmail.com"
 git config --global user.name "Travis-CI"
 
-#Set up authentication
+echo "Set up authentication"
 echo "https://$HEROKU_API_KEY:@github.com" > .git/credentials
-git config credential.helper "store --file=.git/credentials"
 
-#Clone Repositoy
-git clone $TO_REPOSITORY $PUBLISH_PATH --branch $BRANCH --verbose --single-branch --quiet
 
-#Clear Items in master branch
+echo "Clone Repositoy"
+git clone $TO_REPOSITORY $PUBLISH_PATH --branch $BRANCH --verbose --quiet
+
+
+echo "Clear Items in master branch"
 rm -r $PUBLISH_PATH/*
 wintersmith build -o $PUBLISH_PATH
 
 
-#Change dir
+echo "Change dir"
 cd $PUBLISH_PATH
 git add --all . --verbose
 
+echo "Set up location for reading creds"
+git config credential.helper "store --file=../.git/credentials"
 
-#Commit and push
+echo "Commit and push"
 git commit -m "Deploy from travis" --dry-run
 git push --repo=$TO_REPOSITORY --force --verbose --dry-run
