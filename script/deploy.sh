38dbf60215183a1b73eb1e28feb0a7085f646b99
Tushar Mathur <tusharmath@gmail.com>
2018-08-04 17:25:41 +0530

    fixup! fixup! chore(deploy): add deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,11 +3,13 @@ SOURCE_BRANCH="develop"
 ORG="tusharmath"
 REPO="tusharmath.github.io"
 CONTENT_DIR="bin"
+CUSTOM_DOMAIN="tusharm.com"
 
 rm -rf bin
 mkdir bin
 yarn build
 REPO_URL="git@github.com:${ORG}/${REPO}.git"
+echo "${CUSTOM_DOMAIN}" > bin/CNAME
 
 # Inside the directory
 cd bin
