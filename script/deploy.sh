2e1e635bc444b3e26cf2409300967cbdf5b1da05
Tushar Mathur <tusharmath@gmail.com>
2018-08-04 17:10:03 +0530

    chore(deploy): add deploy script
    Signed-off-by: Tushar Mathur <tusharmath@gmail.com>

---
+++ script/deploy.sh
@@ -0,0 +1,20 @@
+DEPLOY_BRANCH="master"
+SOURCE_BRANCH="develop"
+ORG="tusharmath"
+REPO="tusharmath.github.io"
+CONTENT_DIR="bin"
+
+rm -rf bin
+mkdir bin
+yarn build
+REPO_URL="git@github.com:${ORG}/${REPO}.git"
+
+
+# Inside the directory
+cd bin
+git init
+git add .
+git commit -am "deploying to gh-pages"
+git remote add origin REPO_URL
+git push --force origin "${DEPLOY_BRANCH}"
+cd ..
