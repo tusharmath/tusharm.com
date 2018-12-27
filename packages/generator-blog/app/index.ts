f600e5933b1073a4bb78e0e0306c6aa7e8b30f87
Tushar Mathur <tusharmath@gmail.com>
2018-12-27 16:13:05 +0530

    feat(generator): add prompts for user input
    affects: generator-blog

--- packages/generator-blog/app/index.ts
+++ packages/generator-blog/app/index.ts
@@ -1,3 +1,70 @@
 import * as Generator from 'yeoman-generator'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import slugify from 'slugify'
 
-export = class BlogGenerator extends Generator {}
+type UserInput = {
+  title: string
+  type: 'Article' | 'Project'
+  slug: string
+  date: Date
+}
+
+const CONTENT_PATH = 'packages/tusharm.com/contents/articles'
+
+export = class BlogGenerator extends Generator {
+  private answers: UserInput = {
+    type: 'Article',
+    title: 'Nothing',
+    slug: 'nothing',
+    date: new Date()
+  }
+  async prompting() {
+    this.answers = (await this.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Title',
+        validate: i => Boolean(i)
+      },
+      {
+        type: 'input',
+        name: 'slug',
+        message: 'Slug',
+        default: (a: {title: string}) => slugify(a.title)
+      },
+      {
+        type: 'input',
+        name: 'date',
+        message: 'Date',
+        default: this.answers.date
+      },
+      {
+        type: 'list',
+        name: 'type',
+        message: 'Type of content',
+        choices: ['Article', 'Project']
+      }
+    ])) as UserInput
+  }
+
+  private slug(title: string) {
+    return
+  }
+
+  async writing() {
+    const blogPath = path.resolve(
+      process.cwd(),
+      CONTENT_PATH,
+      this.answers.slug
+    )
+
+    await fs.mkdirp(blogPath)
+
+    this.fs.copyTpl(
+      this.templatePath('index.md.tmp'),
+      this.destinationPath(path.resolve(CONTENT_PATH, 'index.md')),
+      this.answers
+    )
+  }
+}
