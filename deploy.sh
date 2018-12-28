fc62b5d86d5b8aa393d7d80646ba80e31f7693e1
Tushar Mathur <tusharmath@gmail.com>
2018-12-28 14:34:51 +0530

    chore(deploy): move deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,20 @@ DEPLOY_BRANCH="master"
 SOURCE_BRANCH="develop"
 ORG="tusharmath"
 REPO="tusharmath.github.io"
-CONTENT_DIR="bin"
+CONTENT_DIR="packages/tusharm.com/bin"
 CUSTOM_DOMAIN="tusharm.com"
 
-rm -rf bin
-mkdir bin
+rm -rf $CONTENT_DIR
+mkdir -p $CONTENT_DIR
 yarn build
 REPO_URL="git@github.com:${ORG}/${REPO}.git"
-echo "${CUSTOM_DOMAIN}" > bin/CNAME
+echo "${CUSTOM_DOMAIN}" > $CONTENT_DIR/CNAME
 
 # Inside the directory
-cd bin
+cd $CONTENT_DIR
 git init
 git add .
 git commit -am "deploying to gh-pages"
 git remote add origin "${REPO_URL}"
 git push --force origin "${DEPLOY_BRANCH}"
-cd ..
+cd -
